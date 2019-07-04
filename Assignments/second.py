5475ef313d359b34925500fa0c261efc2a8f91c9
Nipurnj62 <nipurnj62@gmail.com>
2019-07-04 22:52:37 +0530

    edit

--- Assignments/second.py
+++ Assignments/second.py
@@ -1,3 +1,4 @@
+# Write a program which can compute the factorial of a given numbers The results should be printed in a comma-separated sequence on a single line.
 
 first_number=int(input("enter a number"))
 fact=1
--- Assignments/third.py
+++ Assignments/third.py
@@ -1,3 +1,5 @@
+#With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+
 number=int(input("enter a number"))
 
 output={i:i*i for i in range(1,number+1)}
